68c16c1a50d662e7c95942a26a2cec393c831dec
niedzwiecki.dev@gmail.com <dawid.niedzwiecki@coders-mill.com>
2017-07-13 22:16:48 +0200

    [#13] Adding components and reformating xml of PlaceDetailsActivity

--- app/src/main/java/com/niedzwiecki/przemyslguide/ui/placeDetails/PlaceDetailsActivity.java
+++ app/src/main/java/com/niedzwiecki/przemyslguide/ui/placeDetails/PlaceDetailsActivity.java
@@ -3,13 +3,19 @@ package com.niedzwiecki.przemyslguide.ui.placeDetails;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.niedzwiecki.przemyslguide.R;
 import com.niedzwiecki.przemyslguide.data.model.Ribot;
 import com.niedzwiecki.przemyslguide.ui.base.BaseActivity;
+import com.niedzwiecki.przemyslguide.ui.maps.MapsActivity;
+
+import timber.log.Timber;
 
 import static com.niedzwiecki.przemyslguide.ui.main.MainActivity.RIBOT_KEY;
 
@@ -18,11 +24,11 @@ import static com.niedzwiecki.przemyslguide.ui.main.MainActivity.RIBOT_KEY;
  */
 
 public class PlaceDetailsActivity extends BaseActivity {
+
     /*
     @BindView(R.id.textOfRibot)
     TextView textView;
 */
-
     private Ribot ribot;
 
     public static Intent getStartIntentRibot(Context context, Ribot event) {
@@ -39,15 +45,19 @@ public class PlaceDetailsActivity extends BaseActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_place_details);
+        getSupportActionBar().hide();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-      /*  setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-*/
         if (getIntent().hasExtra(RIBOT_KEY)) {
             ribot = (Ribot) getIntent().getSerializableExtra(RIBOT_KEY);
+
+            FloatingActionButton fabButton = (FloatingActionButton) findViewById(R.id.fabButton);
+            fabButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(PlaceDetailsActivity.this, "FAB CLICK", Toast.LENGTH_SHORT).show();
+                    startActivity(MapsActivity.class);
+                }
+            });
 
             /*textView.setText(String.format("%s %s",
                     ribot.profile().name().first(), ribot.profile().name().last()));*/
