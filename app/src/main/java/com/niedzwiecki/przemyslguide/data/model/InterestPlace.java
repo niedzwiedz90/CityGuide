dfead962ded2a2923e7226c165d5b455e4a6c30a
Niedzwiecki <niedzwiecki.dev@gmail.com>
2017-10-16 22:38:33 +0200

    [#26] Added onClick on marker

--- app/src/main/java/com/niedzwiecki/przemyslguide/data/model/InterestPlace.java
+++ app/src/main/java/com/niedzwiecki/przemyslguide/data/model/InterestPlace.java
@@ -7,12 +7,68 @@ import java.io.Serializable;
  */
 
 public class InterestPlace implements Serializable {
-
     public int id;
     public String image;
     public String name;
     public String address;
     public double longLocation;
     public double latLocation;
+
+    public InterestPlace(int id, String image, String name, String address, double longLocation, double latLocation) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.address = address;
+        this.longLocation = longLocation;
+        this.latLocation = latLocation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLongLocation() {
+        return longLocation;
+    }
+
+    public void setLongLocation(double longLocation) {
+        this.longLocation = longLocation;
+    }
+
+    public double getLatLocation() {
+        return latLocation;
+    }
+
+    public void setLatLocation(double latLocation) {
+        this.latLocation = latLocation;
+    }
 
 }
