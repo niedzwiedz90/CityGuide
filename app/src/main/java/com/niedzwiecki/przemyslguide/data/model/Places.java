6c35bf8d4718d961d27465597335eb1474f180a2
Niedzwiecki <niedzwiecki.dev@gmail.com>
2017-11-01 19:06:37 +0100

    Added header application/Json

---
+++ app/src/main/java/com/niedzwiecki/przemyslguide/data/model/Places.java
@@ -0,0 +1,27 @@
+package com.niedzwiecki.przemyslguide.data.model;
+
+import java.util.List;
+
+/**
+ * Created by Niedzwiecki on 11/1/2017.
+ */
+
+public class Places {
+
+    List<Place> places;
+
+    /*
+            "url": "http://127.0.0.1:8000/api/places/3/?format=json",
+            "lat": 49.821389,
+            "lon": 22.861389,
+            "type": "fort",
+            "name": "Fort GW XIII âSan Rideauâ",
+            "description": "Fort powstaĹ w latach 1892-1896, w miejscu pierwotnie przewidzianym w 1876 pod budowÄ ziemno-drewnianej baterii 11 âBolestraszyceâ, ostatecznie niezrealizowanej. Koszt budowy fortu wyniĂłsĹ 5 milionĂłw koron. Do czasu zmiany numeracji fortĂłw (po 1903) fort oznaczano jako XIII 1/2 âSan Rideauâ. MiaĹ on ksztaĹt trapezu o podstawie 250 m i szerokoĹci 90 m. OtaczaĹa go sucha fosa o peĹnym profilu broniona z dwĂłch przeciwstokowych kojcĂłw oraz kojca szyjowego przylegĹego do gĹĂłwnego budynku fortu. CzoĹowy odcinek fosy broniono dodatkowo z galerii strzeleckiej przeciwstoku z otworami strzelniczymi zabezpieczonymi pĹytami pancernymi (mur Carnota). Na przedpolu nad kojcami znajdowaĹy siÄ dwie obserwacyjne kopuĹy pancerne. GĹĂłwny budynek i kojce przeciwstokowe byĹy poĹÄczone za pomocÄ poterny i galerii przeciwstokowej.\r\n\r\nW czÄĹci szyjowej znajdowaĹ siÄ gĹĂłwny budynek koszarowo-bojowy. Na jego stropodachu znajdowaĹy siÄ 3 haubice i 3 moĹşdzierze, a dodatkowo umieszczono tam rĂłwnieĹź 3 wieĹźe obserwacyjne. Na skrzydĹach budynku znajdowaĹy siÄ tradytory strzeleckie z armatami. Przed stanowiskiem wieĹź pancernych znajdowaĹ siÄ waĹ piechoty ze schronem pogotowia na osi fortu. Na stropie schronu pogotowia znajdowaĹa siÄ betonowa galeria strzelecka osĹoniÄta pĹytami pancernymi. Budynek koszarowo-bojowy mieĹciĹ izby mieszkalne dla zaĹogi, magazyny amunicji, ĹźywnoĹci, laboratoria, toalety i kuchnie. Ĺciany i stropy wykonano jako betonowy odlew z wyjÄtkiem Ĺcian zewnÄtrznych znajdujÄcych siÄ po stronie dziedziĹca fortu, ktĂłre zbudowano z cegĹy. CaĹkowita powierzchnia pomieszczeĹ fortecznych wynosiĹa 2200 mÂ˛.\r\n\r\nPodlegaĹy mu dwa forty pomocnicze â XIIIa âZabĹocieâ oraz XIIIb âBolestraszyceâ. GĹĂłwnym zadaniem fortu byĹo osĹanianie linii rzeki San od strony wschodniej oraz osĹona drĂłg komunikacyjnych (linia kolejowa oraz szosa) ĹÄczÄcych PrzemyĹl z Krakowem, a zasiÄg jego dziaĹ pozwalaĹ na kontrolÄ nad brzegami Sanu od Hurka po WalawÄ.\r\n\r\nPodczas II oblÄĹźenia twierdzy PrzemyĹl, fort zostaĹ wysadzony 22 marca 1915, w dniu poddania twierdzy. UlegĹ kolejnym zniszczeniom podczas III oblÄĹźenia (30 maja 1915) i po kolejnych rozbiĂłrkach w latach 1923-1925. Dalsze niszczenie fortu zostaĹo powstrzymane dopiero w roku 1968, po uznaniu fortĂłw Twierdzy PrzemyĹl za chroniony prawem zabytek architektury.",
+            "image": "https://pl.wikipedia.org/wiki/Fort_GW_XIII_%E2%80%9ESan_Rideau%E2%80%9D#/media/File:San_Rideau_1915_Destroyed.jpg",
+            "email": "",
+            "telephone": "",
+            "created_at": "2017-10-27T17:04:16.677194Z",
+            "images": []
+}*/
+
+}
